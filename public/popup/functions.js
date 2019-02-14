7204fdaff37e66e0fda0ad9d4e2b8029ac3112b1
Andrew Ward <tmiandreww@gmail.com>
2019-02-14 15:47:53 -0500

    get everything working

---
+++ public/popup/functions.js
@@ -0,0 +1,36 @@
+
+$(document).ready(() => {
+  chrome.tabs.query({ active: true }, function (tabs) {
+    let name = tabs[0].title;
+    $('#add-current-tab').text(`Add '${name}'`)
+  });
+  addCurrentTab()
+})
+
+function addCurrentTab() {
+  $(document).on('click', '#add-current-tab', function () {
+    chrome.tabs.query({ active: true }, function (tabs) {
+      console.log(tabs)
+      let url = tabs[0].url;
+      let name = tabs[0].title;
+      let image_url = tabs[0].favIconUrl;
+      if (image_url == null) {
+        image_url = './img/default.png'
+      }
+      addTab(name, url, image_url);
+    });
+
+    $(this).text("Saved!").toggleClass('btn-success');
+  });
+}
+
+function addTab(name, link, img) {
+  chrome.storage.sync.get(["links"], function (obj) {
+    obj.links.push({ "name": name, "actual_link": link, "image_link": img });
+
+    chrome.storage.sync.set({ links: obj.links }, function () {
+      console.log(obj.links);
+      console.log("Added new tab");
+    });
+  });
+}
